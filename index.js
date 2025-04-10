83134aa0cc74699790897318510d5d8f8f7d66f0
Krii41 <134600504+Krii41@users.noreply.github.com>
2025-04-10 20:20:02 +0200

    Modal box

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const navMenu = document.querySelector(".navigation");
 const navToggle = document.querySelector(".mobile-nav");
+const modal = document.getElementById("modal");
+const ctaBtn = document.getElementById("cta-btn");
+const sendBtn = document.getElementById("send-btn");
+
 
 navToggle.addEventListener("click", () => {
     const visibility = navMenu.getAttribute("data-visible");
@@ -13,3 +17,27 @@ navToggle.addEventListener("click", () => {
     }
 
 });
+
+
+ctaBtn.addEventListener("click", function(){
+    modal.innerHTML = `
+    <button class="btn-close" id="btn-close">
+        <i class="fa-solid fa-xmark"></i></button>
+    <h2>Contact Us</h2>
+    <form action="form-success.php" method="post">
+    <input type="text" placeholder="Name">
+    <input type="text" placeholder="E-mail address">
+    <label for="message">Message</label>
+    <textarea name="message" id="message" cols="30" rows="10"></textarea>
+    <button class="send-btn" id="send-btn">Send</button>
+</form>  `
+;
+    modal.style.display = "flex";
+    
+    const btnClose = document.getElementById("btn-close");
+    btnClose.addEventListener("click", function(){
+        modal.style.display = "none";
+    });
+
+});
+
