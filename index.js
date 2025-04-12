a3e481332c76c0da084369eb160c722a840e07a3
Krii41 <134600504+Krii41@users.noreply.github.com>
2025-04-12 19:21:57 +0200

    Meta tags, finished message box

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const navMenu = document.querySelector(".navigation");
 const navToggle = document.querySelector(".mobile-nav");
 const modal = document.getElementById("modal");
 const ctaBtn = document.getElementById("cta-btn");
-const sendBtn = document.getElementById("send-btn");
+
 
 
 navToggle.addEventListener("click", () => {
@@ -24,19 +24,37 @@ ctaBtn.addEventListener("click", function(){
     <button class="btn-close" id="btn-close">
         <i class="fa-solid fa-xmark"></i></button>
     <h2>Contact Us</h2>
-    <form action="form-success.php" method="post">
-    <input type="text" placeholder="Name">
-    <input type="text" placeholder="E-mail address">
-    <label for="message">Message</label>
-    <textarea name="message" id="message" cols="30" rows="10"></textarea>
-    <button class="send-btn" id="send-btn">Send</button>
-</form>  `
+    <form id="contact-form" action="form-success.php" method="post">
+        <input type="text" placeholder="Name">
+        <input type="text" placeholder="E-mail address">
+        <label for="message">Message</label>
+        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        <button class="send-btn" id="send-btn">Send</button>
+    </form>  
+    `
 ;
     modal.style.display = "flex";
-    
+
     const btnClose = document.getElementById("btn-close");
+    const sendBtn = document.getElementById("send-btn");
+    const contactForm = document.getElementById("contact-form");
+
     btnClose.addEventListener("click", function(){
         modal.style.display = "none";
+    });
+
+    contactForm.addEventListener("submit",function(e) {
+        e.preventDefault();
+    });
+
+    sendBtn.addEventListener("click", function(){
+        modal.innerHTML = `
+        <p>Your message has been sent successfully.</p>
+        `
+        ;
+        setTimeout(function() {
+            modal.style.display = "none";
+          }, 1500);
     });
 
 });
